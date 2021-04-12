45559779056a1036c22b3127fa91142b8700dd3c
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-04-11 23:23:27 -0300

    Working robot movement

--- MartianRobot/MartianRobot.ConsoleApp/Program.cs
+++ MartianRobot/MartianRobot.ConsoleApp/Program.cs
@@ -2,14 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MartianRobot.ConsoleApp
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //1. inicializar grid
+            //2. inicializar posicao do robo. posicao deve ser valida
+            //3. mandar a string de movimento
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("@=@=@=@=@=@=@=@=@ MARTIAN ROBOT @=@=@=@=@=@=@=@=@");
+
+            Robot robot1 = new Robot(1, 2, "N");
+
+            //EM EM EM E MM
+            robot1.rotateLeft(); //e
+            robot1.moveAhead();  //m
+
+            robot1.rotateLeft(); //e
+            robot1.moveAhead();  //m
+
+            robot1.rotateLeft(); //e
+            robot1.moveAhead();  //m
+
+            robot1.rotateLeft(); //e
+
+            robot1.moveAhead();  //m
+            robot1.moveAhead();  //m
+
+            Console.WriteLine(robot1.AtributesToString());
+
+            Console.ReadLine();
+
+            
         }
     }
 }
---
+++ MartianRobot/MartianRobot.ConsoleApp/Robot.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartianRobot.ConsoleApp
+{
+    class Robot
+    {
+        public int xPosition;
+        public int yPosition;
+        public string facingDirection;
+
+        public Robot(int xPosition, int yPosition, string facingDirection)
+        {
+            this.xPosition = xPosition;
+            this.yPosition = yPosition;
+            this.facingDirection = facingDirection;
+        }
+
+        public void moveAhead()
+        {
+            switch (facingDirection.ToUpper())
+            {
+                case "N":
+                    yPosition++;
+                    break;
+
+                case "S":
+                    yPosition--;
+                    break;
+
+                case "L":
+                    xPosition--;
+                    break;
+
+                case "O":
+                    xPosition++;
+                    break;
+
+            }
+        }
+
+        public void rotateLeft()
+        {
+            switch (facingDirection.ToUpper())
+            {
+                case "N":
+                    facingDirection = "L";
+                    break;
+
+                case "S":
+                    facingDirection = "O";
+                    break;
+
+                case "L":
+                    facingDirection = "S";
+                    break;
+
+                case "O":
+                    facingDirection = "N";
+                    break;
+                
+            }
+        }
+
+        public void rotateRight()
+        {
+            switch (facingDirection.ToUpper())
+            {
+                case "N":
+                    facingDirection = "O";
+                    break;
+
+                case "S":
+                    facingDirection = "L";
+                    break;
+
+                case "L":
+                    facingDirection = "N";
+                    break;
+
+                case "O":
+                    facingDirection = "S";
+                    break;
+
+            }
+        }
+
+        public string AtributesToString()
+        {
+            return $"{xPosition},{yPosition},{facingDirection}";
+        }
+    }
+}
